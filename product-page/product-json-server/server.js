1623411b242b1218556d584f5d76eb7ae3c5bf1e
Tran Nguyen <tcdnguyen1997@gmail.com>
2019-03-06 23:32:41 +0700

    constructServer-with-json-server

---
+++ product-page/product-json-server/server.js
@@ -0,0 +1,11 @@
+const path = require('path')
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const middlewares = jsonServer.defaults()
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
+
+server.use(middlewares)
+server.use(router)
+server.listen(8080, () => {
+  console.log('JSON Server is running')
+})
